0c0645286bbaeef525f0858225f38b4a9331d566
manojbv966 <manojbv966@gmail.com>
2026-02-05 21:11:03 +0530

    done

--- lab.java
+++ lab.java
@@ -3,6 +3,7 @@ package Cloud_Optima;
 public class lab {
     public static void main(String[] args) {
         System.out.println("Hello, Cloud Opt");
+        System.out.println("This is a lab for Cloud Optima");
     }
 }
     
